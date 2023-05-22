f4834028076d9cc6a4cd635b9fc59e2e6ca986bc
rokon <rokonrabbi21@gmail.com>
2023-05-22 10:43:51 +0600

    gitignore problem

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
+
+
 const port = process.env.port || 3000;
+
+const helmet = require('helmet');
+
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      imgSrc: ["'self'", 'data:'],
+    },
+  })
+);
+app.use(helmet());
 
 app.use(cors());
 app.use(express.json());
@@ -19,47 +34,99 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
 
-    await client.connect();
+
+     client.connect();
     const DisneyToyCollection = client.db("toyDB").collection("allToy");
 
 
     //insertion of toy data
-    app.post('/toys', async (req, res) => {
+    app.post('/Toys', async (req, res) => {
       const newToy = req.body;
       console.log(newToy);
-      const result = await toysCollection.insertOne(newToy);
+      const result = await DisneyToyCollection.insertOne(newToy);
       res.send(result);
     })
 
-
-    app.get("/toy", async (req, res) => {
-      const cursor = toyCollection.find();
+    // get all toys 
+    app.get("/AllDisneyToys", async (req, res) => {
+      const cursor = DisneyToyCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-     //get toy data
-     app.get('/toys', async (req, res) => {
-      console.log(req.query.email);
-      console.log(req.query.category);
+     //get some toy data
+     app.get('/DisneyToys', async (req, res) => {
+      console.log(req.query.sellerEmail);
+      console.log(req.query.subCategory);
       let query = {};
-      if (req.query?.email) {
-        query = {email: req.query.email}
+      if (req.query?.sellerEmail) {
+        query = {sellerEmail: req.query.sellerEmail}
       }
 
-      if (req.query?.category) {
-        query = {category: req.query.category}
+      if (req.query?.subCategory) {
+        query = {subCategory: req.query.subCategory}
       }
-      const result = await DisneyToyCollection.find(query).toArray();
+      const sortField = req.query.sort || 'asc'; // Default to ascending order if no sort parameter is provided
+      const sortOption = sortField === 'asc' ? 1 : -1;
+      const result = await DisneyToyCollection.find(query).sort({ price: sortOption }).toArray();
+      
       res.send(result);
     })
 
+        //Update added here
+        app.get('/DisneyToys/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await DisneyToyCollection.findOne(query);
+          res.send(result);
+        })
+        app.put('/DisneyToys/:id', async (req, res) => {
+          const id = req.params.id;
+          const filter = { _id: new ObjectId(id) }
+          const options = { upsert: true };
+          const updatedToy = req.body;
+          const toy = {
+            $set: {
+              name: updatedToy.name,
+              sellerName: updatedToy.sellerName,
+              sellerEmail: updatedToy.sellerEmail,
+              pictureURL: updatedToy.pictureURL,
+              subCategory: updatedToy.subCategory,
+              price: updatedToy.price,
+              rating: updatedToy.rating,
+              quantity: updatedToy.quantity,
+              description: updatedToy.description
+            }
+          }
+    
+          const result = await DisneyToyCollection.updateOne(filter, toy, options);
+          res.send(result);
+        })
+    
 
+    //Read data
+    app.get('/Disneyalltoys', async (req, res) => {
+      const search = req.query.search || ''; // Get the search query from request parameters
+      const searchRegex = new RegExp(search, 'i'); // Create a case-insensitive 
+    
+      const query = { name: searchRegex }; // Add the search condition to the query
+      const cursor = DisneyToyCollection.find(query).limit(20); 
+      // Limit the 20 result
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    //Delete Operation
+    app.delete('/DisneyToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await DisneyToyCollection.deleteOne(query);
+      res.send(result);
+    })
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
 
 
     // Send a ping to confirm a successful connection
@@ -80,3 +147,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
+
+
